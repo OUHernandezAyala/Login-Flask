8b1fd2c248c8db772d1758dd39ec8c6b492de123
OUHernandezAyala <142889499+OUHernandezAyala@users.noreply.github.com>
2024-01-15 04:51:45 +0000

    Sing up login option

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -7,7 +7,7 @@ import { Home } from "./pages/home";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import {Login} from "./pages/login";
-import {Singup} from "./pages/singup";
+import { Signup } from "./pages/singup";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
@@ -35,7 +35,7 @@ const Layout = () => {
                         <Route element={<Home />} path="/" />
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Login />} path="/login" />
-                        <Route element={<Singup />} path="/singup" />
+                        <Route element={<Signup />} path="/singup" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
--- src/front/js/pages/singup.js
+++ src/front/js/pages/singup.js
@@ -1,55 +1,74 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const Singup = () => {
-	const { store, actions } = useContext(Context);
+export const Signup = () => {
+    const { store, actions } = useContext(Context);
     const [newUser, setNewUser] = useState({
-        email: "",
-        password: "",
-        name:""
+        email: null,
+        password: null,
+        name: null,
     });
+    const [signupSuccess, setSignupSuccess] = useState(false);
 
     const handleChange = (event) => {
         const { id, value } = event.target;
         setNewUser((prev) => ({
             ...prev,
-            [id]: value
+            [id]: value,
         }));
     };
 
-    const handleSingup = async (event) => {
-        event.preventDefault(); 
-        console.log(newUser)
+    const handleSignup = async (event) => {
+        event.preventDefault();
+        if (!newUser.name || !newUser.email || !newUser.password) {
+            alert("Please fill in all required fields");
+            return;
+        }
+
+        console.log(newUser);
         const data = await actions.postNewUser(newUser);
-        console.log(data)
+        console.log(data);
+        setSignupSuccess(true);
     };
 
-
-	return (
+    return (
         <div className="d-flex flex-column align-items-center">
             <form className="m-5">
                 <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-                    <input type="name" className="form-control" id="name" aria-describedby="nameHelp" onChange={handleChange}/>
+                    <label htmlFor="name" className="form-label">
+                        Name
+                    </label>
+                    <input type="name" className="form-control" id="name" aria-describedby="nameHelp" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="email" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={handleChange}/>
+                    <label htmlFor="email" className="form-label">
+                        Email address
+                    </label>
+                    <input type="email" className="form-control" id="email" aria-describedby="emailHelp" onChange={handleChange} />
                 </div>
                 <div className="mb-3">
-                    <label htmlFor="password" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="password" onChange={handleChange}/>
+                    <label htmlFor="password" className="form-label">
+                        Password
+                    </label>
+                    <input type="password" className="form-control" id="password" onChange={handleChange} />
                 </div>
-                <button className="btn btn-primary" onClick={handleSingup}>Submit</button>  
+                {signupSuccess ? (
+                    <>
+                        <p className="text-success">Signup successful! Please proceed to login.</p>
+                        <Link to="/login">
+                            <button className="btn btn-primary mx-1">Login</button>
+                        </Link>
+                    </>
+                ) : (
+                    <button className="btn btn-primary" onClick={handleSignup}>
+                        Submit
+                    </button>
+                )}
             </form>
             <Link to="/" className="mt-3">
                 <button className="btn btn-primary">Back home</button>
             </Link>
         </div>
-
-
-
-       
-	);
+    );
 };
